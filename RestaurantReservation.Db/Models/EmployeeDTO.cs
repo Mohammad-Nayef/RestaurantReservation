63dc6761388e485f9d3ce7817e872f70e722036a
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-09 22:40:55 +0300

    Created the properties of EmployeeDTO

--- RestaurantReservation.Db/Models/EmployeeDTO.cs
+++ RestaurantReservation.Db/Models/EmployeeDTO.cs
@@ -5,5 +5,19 @@ namespace RestaurantReservation.Db.Models
     [Table("Employees")]
     public class EmployeeDTO
     {
+        [Column("employee_id")]
+        public int EmployeeId { get; set; }
+
+        [Column("restaurant_id")]
+        public int RestaurantId { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("lat_name")]
+        public string LastName { get; set; }
+
+        [Column("position")]
+        public string Position { get; set; }
     }
 }
