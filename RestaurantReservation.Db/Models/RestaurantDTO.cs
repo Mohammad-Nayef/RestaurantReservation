efd91c55a8ecd8d9d109d3a5fb020758f68eebdc
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-09 22:10:36 +0300

    Created the properties of RestaurantDTO

--- RestaurantReservation.Db/Models/RestaurantDTO.cs
+++ RestaurantReservation.Db/Models/RestaurantDTO.cs
@@ -5,5 +5,19 @@ namespace RestaurantReservation.Db.Models
     [Table("Restaurants")]
     public class RestaurantDTO
     {
+        [Column("restaurant_id")]
+        public int RestaurantId { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("address")]
+        public string Address { get; set; }
+
+        [Column("phone_number")]
+        public string PhoneNumber { get; set; }
+
+        [Column("opening_hours")]
+        public string OpeningHours { get; set; }
     }
 }
