3eb7c552f97f830ec4d2be798d6b981827fa6397
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-09 22:26:31 +0300

    Created the properties of MenuItemDTO

--- RestaurantReservation.Db/Models/MenuItemDTO.cs
+++ RestaurantReservation.Db/Models/MenuItemDTO.cs
@@ -5,5 +5,19 @@ namespace RestaurantReservation.Db.Models
     [Table("MenuItems")]
     public class MenuItemDTO
     {
+        [Column("item_id")]
+        public int MenuItemId { get; set; }
+
+        [Column("restaurant_id")]
+        public int RestaurantId { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("description")]
+        public string? Description { get; set; }
+
+        [Column("price")]
+        public decimal Price { get; set; }
     }
 }
