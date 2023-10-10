6747476c1285a6132bcf494892fddf5c0d79a9da
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-10 10:51:59 +0300

    Created the properties of CustomerDTO

--- RestaurantReservation.Db/Models/CustomerDTO.cs
+++ RestaurantReservation.Db/Models/CustomerDTO.cs
@@ -5,6 +5,19 @@ namespace RestaurantReservation.Db.Models
     [Table("Customers")]
     public class CustomerDTO
     {
+        [Column("customer_id")]
+        public int CustomerID { get; set; }
 
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("phone_number")]
+        public string PhoneNumber { get; set; }
     }
 }
