3a4b0a0d104f4a7b79635bd9b954e6084d7b719c
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-10 15:00:13 +0300

    Created the properties which are the tables

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -1,7 +1,21 @@
-﻿namespace RestaurantReservation.Db
-{
-    public class RestaurantReservationDbContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
 
+namespace RestaurantReservation.Db
+{
+    public class RestaurantReservationDbContext : DbContext
+    {
+        public DbSet<CustomerDTO> Customers { get; set; }
+        public DbSet<EmployeeDTO> Employees { get; set; }
+        public DbSet<MenuItemDTO> MenuItems { get; set; }
+        public DbSet<OrderDTO> Orders { get; set; }
+        public DbSet<OrderItemDTO> OrderItems { get; set; }
+        public DbSet<RestaurantDTO> Restaurants { get; set; }
+        public DbSet<TableDTO> Tables { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
     }
 }
