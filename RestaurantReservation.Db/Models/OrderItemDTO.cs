99422a2d6b5e6bd29beac3130e80a3ecb8503f13
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-10 11:07:29 +0300

    Created the properties of OrderItemDTO

---
+++ RestaurantReservation.Db/Models/OrderItemDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantReservation.Db.Models
+{
+    [Table("OrderItems")]
+    public class OrderItemDTO
+    {
+        [Column("order_item_id")]
+        public int OrderItemId { get; set; }
+
+        [Column("order_id")]
+        public int OrderId { get; set; }
+
+        [Column("item_id")]
+        public int ItemId { get; set; }
+
+        [Column("quantity")]
+        public int Quantity { get; set; }
+    }
+}
