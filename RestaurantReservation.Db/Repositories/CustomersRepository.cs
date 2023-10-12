508f1d9cf129526d5f61f0185e282daf5390a036
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-12 18:03:56 +0300

    Created method for retrieving a single customer from the database

--- RestaurantReservation.Db/Repositories/CustomersRepository.cs
+++ RestaurantReservation.Db/Repositories/CustomersRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace RestaurantReservation.Db.Repositories
-{
-    public class CustomersRepository
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
 
+namespace RestaurantReservation.Db.Repositories
+{
+    public static class CustomersRepository
+    {
+        public static async Task<Customer?> GetAsync(int customerId)
+        {
+            using (var context = new RestaurantReservationDbContext())
+            {
+                return await context.Customers
+                    .SingleOrDefaultAsync(customer => customer.Id == customerId);
+            }
+        }
     }
 }
