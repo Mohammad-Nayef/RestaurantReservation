c179f676cbb6d6031b687a51dd644dba23325348
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-13 17:34:28 +0300

    Created CRUD methods

--- RestaurantReservation.Db/Repositories/MenuItemsRepository.cs
+++ RestaurantReservation.Db/Repositories/MenuItemsRepository.cs
@@ -1,7 +1,56 @@
-﻿namespace RestaurantReservation.Db.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Db.Repositories
 {
     public class MenuItemsRepository
     {
+        private RestaurantReservationDbContext _context;
 
+        public MenuItemsRepository(RestaurantReservationDbContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreatedAsync().Wait();
+        }
+
+        public async Task<int> CreateAsync(MenuItem newMenuItem)
+        {
+            var menuItem = await _context.MenuItems.AddAsync(newMenuItem);
+            await _context.SaveChangesAsync();
+            return menuItem.Entity.Id;
+        }
+
+        public async Task<MenuItem> GetAsync(int menuItemId)
+        {
+            var menuItem = await _context.MenuItems
+                .SingleOrDefaultAsync(item => item.Id == menuItemId);
+
+            if (menuItem != null)
+            {
+                return menuItem;
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Menu item with ID = {menuItemId} does not exist.");
+            }
+        }
+
+        public async Task<List<MenuItem>> GetAllAsync()
+        {
+            return await _context.MenuItems.ToListAsync();
+        }
+
+        public async Task UpdateAsync(MenuItem updatedMenuItem)
+        {
+            _context.MenuItems.Update(updatedMenuItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int menuItemId)
+        {
+            var menuItem = await GetAsync(menuItemId);
+            _context.MenuItems.Remove(menuItem);
+            await _context.SaveChangesAsync();
+        }
     }
 }
