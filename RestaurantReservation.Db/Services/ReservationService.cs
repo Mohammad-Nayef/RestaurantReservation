ee08f77a1c67cf2f9cbc64aeab97d66cd474f0de
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-13 19:38:45 +0300

    Created ReservationService class

---
+++ RestaurantReservation.Db/Services/ReservationService.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantReservation.Db.Models;
+using RestaurantReservation.Db.Repositories;
+
+namespace RestaurantReservation.Db.Services
+{
+    public class ReservationService
+    {
+        private RestaurantReservationDbContext _context = new();
+        private ReservationsRepository reservationsRepository;
+
+        public ReservationService()
+        {
+            reservationsRepository = new(_context);
+        }
+
+        ~ReservationService()
+        {
+            _context.DisposeAsync();
+        }
+    }
+}
