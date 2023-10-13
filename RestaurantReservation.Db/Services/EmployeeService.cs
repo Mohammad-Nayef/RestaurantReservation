38280809dfd0085a2d8812e2236f24699e4d5205
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-13 18:24:51 +0300

    Created EmployeeService class

---
+++ RestaurantReservation.Db/Services/EmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using RestaurantReservation.Db.Models;
+using RestaurantReservation.Db.Repositories;
+
+namespace RestaurantReservation.Db.Services
+{
+    public class EmployeeService
+    {
+        private RestaurantReservationDbContext _context = new();
+        private EmployeesRepository employeesRepository;
+
+        public EmployeeService()
+        {
+            employeesRepository = new(_context);
+        }
+    }
+}
