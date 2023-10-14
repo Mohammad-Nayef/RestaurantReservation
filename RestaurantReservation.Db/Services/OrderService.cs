0c9024d11e2a3cd3ca8bddba49e55e66ac862c0c
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-14 11:56:34 +0300

    Created OrderServer class

---
+++ RestaurantReservation.Db/Services/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using RestaurantReservation.Db.Repositories;
+
+namespace RestaurantReservation.Db.Services
+{
+    public class OrderService
+    {
+        private RestaurantReservationDbContext _context = new();
+        private OrdersRepository ordersRepository;
+
+        public OrderService()
+        {
+            ordersRepository = new(_context);
+        }
+
+        ~OrderService()
+        {
+            _context.DisposeAsync();
+        }
+    }
+}
