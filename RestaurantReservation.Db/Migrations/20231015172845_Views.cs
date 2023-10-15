3435001d3826f124a8a0020bf3a9e78549083f15
mohammad-nayef <awladmohammad20@gmail.com>
2023-10-15 20:49:44 +0300

    Created view for reservations with their customers and restaurants

---
+++ RestaurantReservation.Db/Migrations/20231015172845_Views.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class Views : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"CREATE VIEW vw_ReservationsWithCustomerAndRestaurant AS
+                    SELECT 
+                        rsr.Id AS ReservationID, rsr.TableId, rsr.ReservationDate, rsr.PartySize,
+                        rsr.CustomerId, cust.FirstName AS CustomerFirstName, cust.LastName AS CustomerLastName,
+                        cust.Email AS CustomerEmail, cust.PhoneNumber AS CustomerPhoneNumber,
+                        rsr.RestaurantId, rest.Name AS RestaurantName, rest.Address AS RestaurantAddress,
+                        rest.PhoneNumber AS RestaurantPhoneNumber, rest.OpeningHours AS RestaurantOpeningHours
+                    FROM Reservations AS rsr
+                    JOIN Customers AS cust
+                    ON rsr.CustomerId = cust.Id
+                    JOIN Restaurants AS rest
+                    ON rsr.RestaurantId = rest.Id"
+                );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW vw_ReservationsWithCustomerAndRestaurant");
+        }
+    }
+}
