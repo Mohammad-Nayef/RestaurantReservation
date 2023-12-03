75379685d39cea3080eb8852acb925b732cf8064
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-03 20:41:50 +0200

    Registered DbContext

--- RestaurantReservation.API/Program.cs
+++ RestaurantReservation.API/Program.cs
@@ -1,3 +1,4 @@
+using RestaurantReservation.Db;
 using RestaurantReservation.Db.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<RestaurantReservationDbContext>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 var app = builder.Build();
