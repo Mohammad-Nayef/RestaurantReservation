c71aa23524d0c9265a35ad14a97c40d48b7a96f8
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-03 17:43:48 +0200

    Created a DTO to hold pagination metadata

---
+++ RestaurantReservation.API/Models/PaginationMetadataDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace RestaurantReservation.API.Models
+{
+    public class PaginationMetadataDTO
+    {
+        public int TotalItemsCount { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPagesCount => (int)Math.Ceiling((double)TotalItemsCount / PageSize);
+
+        public PaginationMetadataDTO(int totalItemCount, int pageSize, int currentPage)
+        {
+            TotalItemsCount = totalItemCount;
+            PageSize = pageSize;
+            CurrentPage = currentPage;
+        }
+    }
+}
