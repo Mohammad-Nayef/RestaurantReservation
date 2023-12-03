0ccfab936f3f6cb6dce5480b8a55f372e9e9490b
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-03 23:18:54 +0200

    Added new model for creating a new customer without an id

---
+++ RestaurantReservation.API/Models/CustomerCreationDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace RestaurantReservation.API.Models
+{
+    /// <summary>
+    /// A customer without Id property for creating a new one.
+    /// </summary>
+    public class CustomerCreationDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
