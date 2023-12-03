362ebe86ad40f8420251ba56822ea9f764ac378d
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-03 22:48:56 +0200

    Creation an action to return a customer with specific id

--- RestaurantReservation.API/Controllers/CustomerController.cs
+++ RestaurantReservation.API/Controllers/CustomerController.cs
@@ -31,10 +31,10 @@ namespace RestaurantReservation.API.Controllers
         /// </summary>
         /// <param name="pageNumber">Number of the page that contains the needed customers.</param>
         /// <param name="pageSize">The size of the needed page.</param>
-        /// <response code="200">List of requested customers with pagination metadata in the headers</response>
+        /// <response code="200">Returns list of the requested customers with pagination metadata in the headers.</response>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<CustomerDTO>), StatusCodes.Status200OK)]
         public async Task<IActionResult> Get(int pageNumber = 1, int pageSize = 10)
         {
             if (pageSize > _pageSizeLimit)
@@ -53,6 +53,32 @@ namespace RestaurantReservation.API.Controllers
             var jsonPaginationMetadata = JsonSerializer.Serialize(paginationMetadata);
 
             Response.Headers.Add("X-Pagination", jsonPaginationMetadata);
+        }
+
+        /// <summary>
+        /// Gets a customer by his Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <response code="200">Returns the requested customer.</response>
+        /// <response code="404">The customer with the given Id doesn't exist.</response>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(CustomerDTO), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get(int id)
+        {
+            Customer customer;
+
+            try
+            {
+                customer = await _customerService.GetAsync(id);
+            }
+            catch (KeyNotFoundException) 
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<CustomerDTO>(customer));
         }
     }
 }
