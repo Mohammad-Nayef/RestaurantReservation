5bbc4eeea32e7e2aec4a22719fe78ae84ed0d53e
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-05 15:38:43 +0200

    Created models for reservation

---
+++ RestaurantReservation.API/Models/ReservationWithoutIdDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantReservation.API.Models
+{
+    public class ReservationWithoutIdDTO
+    {
+        public int CustomerId { get; set; }
+        public int RestaurantId { get; set; }
+        public int TableId { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public int PartySize { get; set; }
+    }
+}
