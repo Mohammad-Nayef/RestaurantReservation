ea0f4630994920ec7f9bd5d5407c56525afab0e4
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-05 13:27:25 +0200

    Added the foreign keys properties

--- RestaurantReservation.API/Models/EmployeeDTO.cs
+++ RestaurantReservation.API/Models/EmployeeDTO.cs
@@ -3,6 +3,7 @@
     public class EmployeeDTO
     {
         public int Id { get; set; }
+        public int RestaurantId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
--- RestaurantReservation.API/Models/EmployeeWithoutIdDTO.cs
+++ RestaurantReservation.API/Models/EmployeeWithoutIdDTO.cs
@@ -2,6 +2,7 @@
 {
     public class EmployeeWithoutIdDTO
     {
+        public int RestaurantId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
