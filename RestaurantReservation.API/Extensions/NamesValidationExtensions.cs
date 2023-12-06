a55222776d77621e84bc753177073bb685f9db27
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-06 23:03:25 +0200

    Created an extension to validate names

---
+++ RestaurantReservation.API/Extensions/NamesValidationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace RestaurantReservation.API.Extensions
+{
+    public static class NamesValidationExtensions
+    {
+        /// <summary>
+        /// Validate name property using FluentValidation.
+        /// </summary>
+        public static IRuleBuilderOptions<T, string> ValidateName<T>(
+            this IRuleBuilder<T, string> rule)
+        {
+            return rule
+                .Matches(@"^[A-Za-z\s]+$").WithMessage(
+                     "'{PropertyName}' should only contain letters and spaces.")
+                .Length(3, 30);
+        }
+    }
+}
