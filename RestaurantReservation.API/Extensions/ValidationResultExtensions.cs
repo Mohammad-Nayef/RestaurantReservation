11b7b8e5283ece9b5d1f8f299473a40340758f20
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-06 23:02:57 +0200

    Created an extension to return specific errors response

---
+++ RestaurantReservation.API/Extensions/ValidationResultExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+
+namespace RestaurantReservation.API.Extensions
+{
+    public static class ValidationResultExtensions
+    {
+        /// <summary>
+        /// Returns only the important properties contained in the errors list.
+        /// </summary>
+        public static object GetImportantProperties(this ValidationResult validationResult)
+        {
+            return validationResult.Errors.Select(error => new
+            {
+                error.ErrorMessage,
+                error.PropertyName,
+                error.AttemptedValue
+            });
+        }
+    }
+}
