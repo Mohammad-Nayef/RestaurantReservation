33924e266d08aaba22400698d87f98c4eeb0c366
mohammad-nayef <awladmohammad20@gmail.com>
2023-12-08 15:11:04 +0200

    Created a method to return menu items of a reservation

--- RestaurantReservation.API/Controllers/ReservationController.cs
+++ RestaurantReservation.API/Controllers/ReservationController.cs
@@ -166,6 +166,44 @@ namespace RestaurantReservation.API.Controllers
         }
 
         /// <summary>
+        /// List ordered menu items for a reservation.
+        /// </summary>
+        /// <param name="reservationId">The Id of the needed reservation</param>
+        /// <param name="pageNumber">Number of the page that contains the needed menu items.</param>
+        /// <param name="pageSize">The size of the needed page.</param>
+        /// <response code="200">Returns the requested menu items.</response>
+        /// <response code="404">The reservation with the given Id doesn't exist.</response>
+        [HttpGet("{reservationId}/menu-items")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(List<MenuItem>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> OrderedMenuItemsByReservation(
+            int reservationId,
+            int pageNumber = 1,
+            int pageSize = 10)
+        {
+            if (pageNumber < 1 || pageSize < 1)
+                return BadRequest(
+                    $"'{nameof(pageNumber)}' and '{nameof(pageSize)}' must be greater than 0.");
+
+            if (pageSize > _pageSizeLimit)
+                pageSize = _pageSizeLimit;
+
+            if (!await _reservationService.ReservationExistsAsync(reservationId))
+                return NotFound();
+
+            var menuItemsOfReservationCount = await _reservationService
+                .GetMenuItemsByReservationCountAsync(reservationId);
+            Response.Headers.AddPaginationMetadata(
+                menuItemsOfReservationCount, pageSize, pageNumber);
+
+            var menuItemsOfReservation = await _reservationService
+                .ListOrderedMenuItemsAsync(reservationId, pageNumber, pageSize);
+
+            return Ok(menuItemsOfReservation);
+        }
+
+        /// <summary>
         /// Create and store a new reservation.
         /// </summary>
         /// <param name="newReservation">Properties of the new reservation.</param>
